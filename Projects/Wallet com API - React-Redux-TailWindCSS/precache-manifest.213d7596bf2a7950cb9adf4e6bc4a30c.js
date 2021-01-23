f8431fa627227a9cea072e51baab9b44dedf03b7
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-23 18:03:08 -0300

    Tentando

---
+++ Projects/Wallet com API - React-Redux-TailWindCSS/precache-manifest.213d7596bf2a7950cb9adf4e6bc4a30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee6446d470a59a18cb4ca2a553f004a",
+    "url": "/Projects/wallet/index.html"
+  },
+  {
+    "revision": "32db1fea17069e446884",
+    "url": "/Projects/wallet/static/css/main.cbc02002.chunk.css"
+  },
+  {
+    "revision": "b306333d783474383da4",
+    "url": "/Projects/wallet/static/js/2.e14674c4.chunk.js"
+  },
+  {
+    "revision": "df34426e3c67cba54df30dae4655a86f",
+    "url": "/Projects/wallet/static/js/2.e14674c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32db1fea17069e446884",
+    "url": "/Projects/wallet/static/js/main.0717031f.chunk.js"
+  },
+  {
+    "revision": "d8ad627258b9ac54c2c4",
+    "url": "/Projects/wallet/static/js/runtime-main.35deba37.js"
+  }
+]);
